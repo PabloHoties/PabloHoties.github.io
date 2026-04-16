227a8e79d0db719959adcedf25ef1a6167832ad1
PabloHoties <pablohotiesmarques@gmail.com>
2026-04-15 21:31:13 -0300

    desenvolvimento de footer

--- src/app/layout/components/footer/footer.ts
+++ src/app/layout/components/footer/footer.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
 
+  discordCopiado: boolean = false;
+  private timeoutId: any;
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  copiarDiscord() {
+    navigator.clipboard.writeText('pablohoties').then(() => {
+
+      this.discordCopiado = true;
+      this.cdr.detectChanges();
+
+      if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+      }
+
+      this.timeoutId = setTimeout(() => {
+        this.discordCopiado = false;
+        this.cdr.detectChanges();
+      }, 2250);
+
+    }).catch(err => {
+      console.error('Falha ao copiar: ', err);
+    });
+  }
 }
